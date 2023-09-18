a576600fda093e00867568e97790de042fe4768b
taky-kongo <abdoulmalickkongo@gmail.com>
2023-09-18 13:30:34 +0000

    Update the file

--- main.h
+++ main.h
@@ -21,5 +21,8 @@ extern char **environ;
 /**tools.c**/
 int _strcmp(const char *s1, const char *s2);
 int _putchar(char c);
+void _puts(char *str);
+int _atoi(char *s);
+char *_strdup(char *str);
 
 #endif
--- tools.c
+++ tools.c
@@ -30,3 +30,67 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * _puts - a function that prints a string
+ * @str: String input
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
+
+/**
+ * _atoi - string to integer converter func
+ * @s: input string
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int i, j, n, m;
+
+	i = n = 0;
+	m = 1;
+	while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+	{
+		if (*(s + i) == '-')
+			m *= -1;
+		i++;
+	}
+	j = 1;
+	while ((*(s + j) >= '0') && (*(s + j) > '9'))
+	{
+		n = n * 10 + m * (*(s + j) - '0');
+		j++;
+	}
+	return (n);
+}
+
+/**
+ * _strdup - Duplicates a string
+ * @str: String to be duplicated
+ *
+ * Return: Pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, len;
+	char *duplicate;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	duplicate = (char *)malloc(sizeof(char) * (len + 1));
+	if (duplicate == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		duplicate[i] = str[i];
+
+	return (duplicate);
+}
