3596e6e763ddb7c5e3ca94ba4e5e05b1d6149eef
taky-kongo <abdoulmalickkongo@gmail.com>
2023-09-20 10:43:36 +0000

    Update the file

--- functions.c
+++ functions.c
@@ -35,3 +35,28 @@ char *_strtok(char *str, const char *separator)
 		a = NULL;
 	return (token);
 }
+
+/**
+ * fill_args - Fill an args_list with every input typed by the user
+ * @input: String with the input
+ * @args_list: args_list to be filled
+ *
+ * Return: Length of the args_list
+ */
+int fill_args(char *input, char **args_list)
+{
+	int i = 0;
+	char *options, *aux = input, *command;
+
+	command = _strtok(input, "\n\t\r ");
+	args_list[i] = command;
+	while (aux != NULL)
+	{
+		++i;
+		options = _strtok(NULL, "\n\t\r ");
+		aux = options;
+		args_list[i] = options;
+	}
+	args_list[i] = NULL;
+	return (i);
+}
